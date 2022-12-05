b578ded94626bb1a79644cc3856bb81f4f3b10b5
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-12-05 17:28:30 +0100

    Documentation improvements

---
+++ docs/book/fix_docs.sh
@@ -0,0 +1,5 @@
+sed -i 'x' 's/<a href=/<a target="frame" href=/g' ./book/index.html
+sed -i 'x' 's/<a href=/<a target="frame" href=/g' ./book/x2.html
+sed -i 'x' 's/<a href=/<a target="frame" href=/g' ./book/x3.html
+sed -i 'x' 's/<a href=/<a target="frame" href=/g' ./book/x4.html
+
