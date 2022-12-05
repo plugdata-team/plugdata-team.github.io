4b5d6a92f170b999083cda7a4557c9fa13bbfb28
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2022-12-05 04:42:32 +0100

    Downloads page improvements

--- download.js
+++ download.js
@@ -3,7 +3,7 @@ function main() {
 
   let title = document.createElement("h2");
   title.innerHTML += "Download plugdata";
-  title.style.cssText = "text-align: center; font-family: InterSemiBold; border-radius:0%; border:none; outline:none; font-size:36px; color: #333333; background-color:transparent";
+  title.style.cssText = "text-align: center; font-family: InterSemiBold; border-radius:0%; border:none; outline:none; font-size:48px; color: #333333; background-color:transparent; margin-top: 70px;";
   content.appendChild(title);
    
    let userAgent = navigator.appVersion;
@@ -49,7 +49,7 @@ function main() {
 
    let other = document.createElement("h1");
    other.innerHTML += "All downloads";
-   other.style.cssText = "top: 50px; text-align: center; font-family: InterSemiBold; padding-top: 160px; font-size:36px; color: #333333; background-color:transparent";
+   other.style.cssText = "top: 50px; text-align: center; font-family: InterSemiBold; padding-top: 160px; padding-bottom: 50px; font-size:36px; color: #333333; background-color:transparent";
    content.appendChild(other);
 
   let downloads = [["macOS Universal", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-MacOS-Universal.zip", "Download"], ["Windows x64", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-Win64.zip", "Download"], ["Windows x86", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-Win32.zip", "Download"], ["Linux Repositories", "https://software.opensuse.org//download.html?project=home%3Aplugdata&package=plugdata", "View"], ["Arch Linux AUR Repositories (stable)", "https://aur.archlinux.org/packages/plugdata-bin", "View"], ["Arch Linux AUR Repositories (latest)", "https://aur.archlinux.org/packages/plugdata-git", "View"], ["More Downloads", "https://github.com/plugdata-team/plugdata/releases/tag/v0.6.2", "View"]];
@@ -57,7 +57,17 @@ function main() {
   for (let i = 0; i < downloads.length; i++) 
   {
     let panel = document.createElement("DIV");
-    panel.style.cssText = "font-family: Inter; position: relative; height: 40px; padding: 1px; margin-top: 1px; width: 600px; left: 50%; transform: translateX(-50%); outline: 1px solid #333333";
+    panel.style.cssText = "font-family: Inter; position: relative; height: 40px; padding: 1px; margin-top: 1px; width: 600px; left: 50%; transform: translateX(-50%); outline: 1px solid #333333;";
+    
+    if(i == 0) {
+      panel.style.cssText += "border-top-left-radius: 9px";
+      panel.style.cssText += "border-top-right-radius: 9px";
+    }
+    if(i == downloads.length - 1) {
+      panel.style.cssText += "border-bottom-left-radius: 9px";
+      panel.style.cssText += "border-bottom-right-radius: 9px";
+    }
+    
     panel.style.zIndex = "1"; 
 
     let name = document.createElement("a");
@@ -65,7 +75,7 @@ function main() {
     name.innerHTML = downloads[i][0];
 
     let link = document.createElement("a");
-    link.style.cssText = "font-family: Inter; position: absolute; right: 5px; width: 200px; top: 50%; transform: translateY(-50%);";
+    link.style.cssText = "font-family: Inter; position: absolute; right: 50px; width: 200px; top: 50%; transform: translateY(-50%);";
     link.href = downloads[i][1];
     link.innerHTML = downloads[i][2];
 
