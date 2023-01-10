007c38d7386a745f270bf0ab6fd9cf298de97734
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-01-10 17:22:49 +0100

    Updated links to latest version

--- scripts/download.js
+++ scripts/download.js
@@ -10,21 +10,21 @@ function main() {
    let osDetails = {
      name: 'Unknown OS',
      icon: 'fa-question-circle',
-     link: 'https://github.com/plugdata-team/plugdata/releases/tag/v0.6.2',
+     link: 'https://github.com/plugdata-team/plugdata/releases/tag/v0.6.3',
      offset: 40
    };
    
    if (userAgent.includes('Macintosh')) {
      osDetails.name = 'macOS';
      osDetails.icon = 'fa-apple';
-     osDetails.link = 'https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-MacOS-Universal.zip';
+     osDetails.link = 'https://github.com/plugdata-team/plugdata/releases/download/v0.6.3/PlugData-MacOS-Universal.zip';
      osDetails.offset = 0;
     }
    
    if (userAgent.includes('Windows')) {
      osDetails.name = 'Windows';
      osDetails.icon = 'fa-windows';
-     osDetails.link = 'https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-Win64.zip';
+     osDetails.link = 'https://github.com/plugdata-team/plugdata/releases/download/v0.6.3/PlugData-Win64.zip';
      osDetails.offset = 5;
    }
    
@@ -70,7 +70,7 @@ function main() {
    stable.style.cssText = "top: 50px; text-align: center; font-family: Inter; font-weight: 500; padding-top: 20px; padding-bottom: 20px; font-size:24px; color: #333333; background-color:transparent";
    content.appendChild(stable);
 
-  let stable_downloads = [["macOS Universal", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-MacOS-Universal.zip", "Download"], ["Windows (x64)", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-Win64.zip", "Download"], ["Windows (x86)", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.2/PlugData-Win32.zip", "Download"], ["Linux Repositories", "https://software.opensuse.org//download.html?project=home%3Aplugdata&package=plugdata", "View"], ["Arch Linux AUR Repository", "https://aur.archlinux.org/packages/plugdata-bin", "View"], ["More Downloads", "https://github.com/plugdata-team/plugdata/releases/tag/v0.6.2", "View"]];
+  let stable_downloads = [["macOS Universal", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.3/PlugData-MacOS-Universal.zip", "Download"], ["Windows (x64)", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.3/PlugData-Win64.zip", "Download"], ["Windows (x86)", "https://github.com/plugdata-team/plugdata/releases/download/v0.6.3/PlugData-Win32.zip", "Download"], ["Linux Repositories", "https://software.opensuse.org//download.html?project=home%3Aplugdata&package=plugdata", "View"], ["Arch Linux AUR Repository", "https://aur.archlinux.org/packages/plugdata-bin", "View"], ["More Downloads", "https://github.com/plugdata-team/plugdata/releases/tag/v0.6.3", "View"]];
 
   let stable_container =  document.createElement("div");
   stable_container.style.margin = "15px";
